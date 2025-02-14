0ec732abf5da681f96814c1dbbb5052743c2045c
Alberto Lima <alberto.btlima@outlook.com>
2025-02-14 19:54:09 -0300

    ajustes no eslint

--- projeto-spotify-clone/front-end/api/api.js
+++ projeto-spotify-clone/front-end/api/api.js
@@ -1,7 +1,7 @@
-import axios from "axios";
 import "dotenv/config";
+import axios from "axios";
 
-const { NODE_ENV } = process.env;
+const NODE_ENV = process.env.NODE_ENV;
 
 const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
 
