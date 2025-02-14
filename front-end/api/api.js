43e43e217ed88f4fb5a346d012b69b11dffc9dbd
Alberto Lima <alberto.btlima@outlook.com>
2025-02-14 20:25:00 -0300

    ajustes no env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import axios from "axios";
 
-const NODE_ENV = process.env.NODE_ENV;
+// const NODE_ENV = process.env.NODE_ENV;
+// const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
 
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const URL = "http://localhost:3000/api";
+
+const URL = "https://jornada-full-stack-2025.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
