a29e77fe49b0ea9f889d4d5be1979ad4cdff7233
Alberto Lima <alberto.btlima@outlook.com>
2025-02-22 11:03:12 -0300

    teste

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,5 +1,8 @@
 import { MongoClient } from "mongodb";
 
+const URI =
+  "mongodb+srv://alberto:P6gDlprQV0opEwT9@cluster0.nb44t.mongodb.net/spotifyAula?retryWrites=true&w=majority&appName=Cluster0";
+
 const client = new MongoClient(URI);
 
 export const database = client.db("spotifyAula");
